94eea9b8d52ed2ccc6d4f4fbbf45b1f3be205f06
MervynLam <mervynlam@yeah.net>
2024-09-16 23:09:06 +0800

    theme

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,24 @@ import DonateMask from './components/DonateMask.vue'
 import { NMessageProvider } from 'naive-ui'
 import AppMessage from './components/AppMessage.vue'
 import LinkArea from './components/LinkArea.vue'
+
+import { NConfigProvider } from 'naive-ui'
+
+const themeOverrides = {
+  common: {
+    primaryColor: '#42B1C7FF',
+    primaryColorHover: '#60C1D5FF'
+  }
+}
 </script>
 
 <template>
-  <MainLayout />
-  <DonateMask />
-  <n-message-provider>
-    <AppMessage />
-  </n-message-provider>
-  <link-area />
+  <n-config-provider :theme-overrides="themeOverrides">
+    <MainLayout />
+    <DonateMask />
+    <n-message-provider>
+      <AppMessage />
+    </n-message-provider>
+    <link-area />
+  </n-config-provider>
 </template>
